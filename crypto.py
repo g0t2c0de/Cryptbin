376d31d74f6f322a98c605b986a9ea83f417d6ae
Obsidian <ObsidianRock@users.noreply.github.com>
2016-11-10 12:59:31 +0000

    made cryto into a module

--- crypto.py
+++ crypto.py
@@ -11,11 +11,12 @@ from base64 import b64decode
 from urllib.request import urlopen
 import json
 import re
+import os
 
 
-def encrypt():
+def encrypt(password, data):
 
-    password = 'pass'
+    salt = os.urandom(16)
 
     iv = os.urandom(16)
 
@@ -24,13 +25,11 @@ def encrypt():
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=(128//8),
-        salt=b'sdfsdfsdfsd',
-        iterations=1000,
+        salt=salt,
+        iterations=10000,
         backend=backend
     )
     key = kdf.derive(password.encode('utf-8'))
-
-    data = 'eyJpdiI6IlZUQVZXTFFLRUxMbWF3MFltcTFMOWc9PSIsInYiOjEsIml0ZXIiOjEwMDAwLCJrcyI6MTI4LCJ0cyI6MTI4LCJtb2RlIjoiZ2NtIiwiYWRhdGEiOiIiLCJjaXBoZXIiOiJhZXMiLCJzYWx0IjoiSEQrQldxdHozbU09IiwiY3QiOiJET2hZamJ5VW0zQ3JaKy9nbnR1S09PenA0VjFTQ1Y5U1lIaTRSTjdJTG00UCJ9'
 
 
     cipher = Cipher(
@@ -43,37 +42,38 @@ def encrypt():
 
     ct = encryptor.update(data.encode('utf-8')) + encryptor.finalize()
     tag = encryptor.tag
-    return ct,iv, tag
 
 
-def decrypt():
+    crypto_options = {}
+    crypto_options['iv'] = iv
+    crypto_options['salt'] = salt
+    crypto_options['iter'] = 10000
+    crypto_options['tag'] = tag
+    crypto_options['ct'] = ct
 
-    ct ,iv, tag = encrypt()
 
-    password = 'pass'
+    return crypto_options
+
+
+def decrypt(password, option):
 
     backend = default_backend()
 
     kdf = PBKDF2HMAC(
         algorithm=hashes.SHA256(),
         length=(128//8),
-        salt=b'sdfsdfsdfsd',
-        iterations=1000,
+        salt=option['salt'],
+        iterations=option['iter'],
         backend=backend
     )
     key = kdf.derive(password.encode('utf-8'))
 
     cipher = Cipher(
         algorithms.AES(key),
-        modes.GCM(iv, tag),
+        modes.GCM(option['iv'], option['tag']),
         backend=backend,
     )
 
     dec = cipher.decryptor()
-    pt = (dec.update(ct) + dec.finalize()).decode('utf-8')
-    print(pt)
-
-
-if __name__ == '__main__':
-
-    decrypt()
+    pt = (dec.update(option['ct']) + dec.finalize()).decode('utf-8')
+    return pt
